d7833abcb47d73fa809345432d400c1cecbd434e
MrCOOL1732 <chitturivamsi194@gmail.com>
2026-05-17 00:55:00 -0400

    Add Akhila's special website

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-      return "Hi, i am  ch vamsi ai, live from GKE! My app is running 24/7 🚀"
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
-      return "OK", 200
+    return "OK", 200
 
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=8080)##
+    app.run(host="0.0.0.0", port=8080)
